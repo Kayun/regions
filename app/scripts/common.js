0a9ee4a3b3a59b9dc990e049a250534c93b8a4c8
Artyom Kayun <kayun.artem@gmail.com>
2015-04-21 23:56:45 +0700

    add menu on main page

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -2,6 +2,10 @@ $(function () {
 
 	'use strict';
 
+	menuCount();
+
+	// слайдер на главной странице
+	//
 	var mainSlider;
 
 	mainSlider = new Swiper ('.swiper-container', {
@@ -15,5 +19,38 @@ $(function () {
 		nextButton: '.main-slider__next',
 		prevButton: '.main-slider__prev'
 	});
+
+	// счетсик в меню, склонение слова "объявлений"
+
+	function menuCount() {
+		var $count = $('.js-menu-count'),
+			txt1 = 'объявление',
+			txt2 = 'объявления',
+			txt3 = 'объявлений';
+
+		$count.each(function () {
+			var num = parseInt($(this).html()),
+				numRes = num % 10;
+
+			if (
+				numRes === 1 &&
+				num !== 11
+				) {
+
+					$(this).next().html(txt1);
+
+			} else if (
+				(numRes === 2 || numRes === 3 || numRes === 4) &&
+				num !== 12 && num !== 13 && num !== 14
+				) {
+
+					$(this).next().html(txt2);
+
+			} else {
+
+				$(this).next().html(txt3);
+			}
+		});
+	}
 
 });
